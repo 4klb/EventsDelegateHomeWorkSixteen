4ce457bd075e73e54b86b3d903f6b05d31468c50
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2021-02-18 01:50:30 +0600

    Добавьте файлы проекта.

---
+++ EventsDelegateHomeWorkSixteen/Program.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ConsoleButton
+            {
+                Boundary = "-",
+                Width = 8,
+                Height = 20,
+                Text = "Button",
+                InsideColor = ConsoleColor.Red,
+                BoundaryColor = ConsoleColor.Yellow  
+            };
+
+            service.Button();
+
+            service.ButtonAction += new Action<int>(Console.WriteLine);
+            service.ButtonAction += Console.WriteLine;
+
+            service.ButtonAction += data => Console.WriteLine(data);
+
+            int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var result = integers.Where(integer => integer > 5);
+
+            foreach(var value in result)
+            {
+                Console.Write($"{value} ");
+            }
+        }
+    }
+}
---
+++ Models/ConsoleButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class ConsoleButton
+    {
+        public event Action<int> ButtonAction;
+        public string Boundary { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public ConsoleColor InsideColor { get; set; }
+        public ConsoleColor BoundaryColor { get; set; }
+
+        public void Button()
+        {
+            PrintButton();
+            ButtonAction?.Invoke(42);
+        }
+
+        public void PrintButton()
+        {
+            string[,] Array = new string[Width, Height];
+            Console.BackgroundColor = BoundaryColor;
+
+            for (int i = 0; i < Width; i++) //row
+            {
+                for (int j = 0; j < Height; j++) //column
+                {
+                    if (i == 0 || j == 0 || i == Width - 1)
+                    {
+                        Array[i, j] = Boundary;
+                        Console.Write(Array[i, j]);
+                        Console.ForegroundColor = InsideColor;
+                    }
+                    else if (j == Width - 1)
+                    {
+                        int cursorPosition = Height - 1;
+                        Console.CursorLeft = cursorPosition;
+                        Console.Write(Boundary);
+                        Console.Write(Array[i, j]);
+                    }
+                    if (i == Width / 3 && j == Height / 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"\t\b{Text}");
+                        Console.ForegroundColor = InsideColor;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+}
